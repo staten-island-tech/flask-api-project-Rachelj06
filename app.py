6f1b3e1fb908970cac9b3c603d11587e1ab69e05
Rachelj06 <rachelj67@nycstudents.net>
2025-05-15 20:09:11 -0400

    s

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ import requests
 
 app = Flask(__name__)
 
-response = requests.get("https://api.spotify.com")
+response = requests.get("https://api.deezer.com/chart/0/tracks?limit=10")
 data = response.json()
 
-print(data)
+for track in data['data']:
+    print(track)
 
 
 @app.route("/")
 def index():
-    """ response = requests.get("https://api.spotify.com")
+    """ response = requests.get("")
     data = response.json()
     song_list = data['results']
     song = [] """
