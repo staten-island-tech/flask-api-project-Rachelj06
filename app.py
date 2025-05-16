3c72679193cff9122d891424edc3859bf216a127
Rachelj06 <rachelj67@nycstudents.net>
2025-05-16 09:26:21 -0400

    d

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import requests
 
 app = Flask(__name__)
 
-response = requests.get("https://api.deezer.com/chart/0/tracks?limit=10")
+response = requests.get("https://api.deezer.com/chart/0/tracks?limit=100")
 data = response.json()
 
 for track in data['data']:
-    print(track)
+    print(f"{track['title']} by {track['artist']['name']}")
+
+
 
 
 @app.route("/")
