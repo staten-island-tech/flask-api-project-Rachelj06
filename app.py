0e62137a5d4bf224533f2d19154c6a6d7028d3d4
Rachelj06 <rachelj67@nycstudents.net>
2025-05-24 16:47:16 -0400

    flask api

--- app.py
+++ app.py
@@ -3,20 +3,28 @@ import requests
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    response = requests.get("https://api.deezer.com/chart/0/tracks?limit=100")
+    response = requests.get("https://api.deezer.com/chart/0/tracks?limit=50")
     data = response.json()
     track_list = data['data']
+
     tracks = []
     for track in track_list:
-        tracks.append(track)
-        title = track.get('title')
-        artist = track.get(['artist']['name'])
-        image = track.get('md5_image')
+        tracks.append({
+            'id': track['id'],
+            'title': track.get('title'),
+            'artist': track.get('artist', {}).get('name'),
+            'image': track.get('album', {}).get('cover_medium')
+        })
 
     return render_template("index.html", tracks=tracks)
+
+@app.route("/track/<int:id>")
+def detail(id):
+    response = requests.get(f"https://api.deezer.com/track/{id}")
+    data = response.json()
+    return render_template("detail.html", track=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
