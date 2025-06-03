a0716e420e2c9fb65616f1237236820669887e99
Rachelj06 <rachelj67@nycstudents.net>
2025-06-03 18:20:21 -0400

    done

--- app.py
+++ app.py
@@ -5,27 +5,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    if request.method == "filter":
-        filter = request.form["filter"]
-
+    query = request.args.get("q", "").lower()
     response = requests.get("https://api.deezer.com/chart/0/tracks?limit=100")
     data = response.json()
     track_list = data['data']
 
     tracks = []
     for track in track_list:
-        tracks.append({
-            'id': track['id'],
-            'title': track.get('title'),
-            'artist': track.get('artist', {}).get('name'),
-            'image': track.get('album', {}).get('cover_medium')
-        })
+        title = track.get("title", "").lower()
+        artist = track.get("artist", {}).get("name", "").lower()
 
-    return render_template("index.html", tracks=tracks)
+        if query in title or query in artist or query == "":
+            tracks.append({
+                'id': track['id'],
+                'title': track.get('title'),
+                'artist': track.get('artist', {}).get('name'),
+                'image': track.get('album', {}).get('cover_medium')
+            })
+            
+    showing_all = False
+    if query and not tracks:
+        showing_all = True
+        for track in track_list:
+            tracks.append({
+                'id': track['id'],
+                'title': track['title'],
+                'artist': track['artist']['name'],
+                'image': track['album']['cover_medium']
+            })
 
-@app.route("/ee")
-def filter(f):
-    e = "e"
+    return render_template("index.html", tracks=tracks, query=query, showing_all=showing_all)
 
 @app.route("/track/<int:id>")
 def detail(id):
